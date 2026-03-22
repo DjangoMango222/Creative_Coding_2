0d912eaff1db5046422909002b0fe80b5829836a
Django Behunin <django.behunin@umconnect.umt.edu>
2026-03-21 23:21:50 -0600

    asdf

--- HW-9/js/sketch.js
+++ HW-9/js/sketch.js
@@ -3,21 +3,20 @@ let apple;
 let badItems;
 let obstacles;
 
-let appleImg, burgerImg;
-let snakeImg;
+let playerImg, appleImg, badImg;
 
 let score = 0;
 let health = 100;
 let gameState = "menu";
 
 function preload() {
-  snakeImg = loadImage("images/snake-head-right.png");
+  playerImg = loadImage("images/snake-head-right.png");
   appleImg = loadImage("images/apple.png");
-  burgerImg = loadImage("images/fast-food-png-41613.png");
+  badImg = loadImage("images/fast-food-png-41613.png");
 }
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
@@ -26,10 +25,10 @@ function draw() {
   if (gameState === "menu") {
     fill(255);
     textSize(40);
-    text("VentureSnake", 240, 200);
+    text("VentureSnake", width / 2 - 120, height / 2 - 40);
 
-    textSize(18);
-    text("Press SPACE to start", 280, 260);
+    textSize(20);
+    text("Press SPACE to start", width / 2 - 110, height / 2 + 10);
   }
 
   if (gameState === "play") {
@@ -38,14 +37,14 @@ function draw() {
 
   if (gameState === "win") {
     fill(255);
-    textSize(30);
-    text("YOU WIN", 320, 300);
+    textSize(40);
+    text("YOU WIN", width / 2 - 100, height / 2);
   }
 
   if (gameState === "lose") {
     fill(255);
-    textSize(30);
-    text("GAME OVER", 300, 300);
+    textSize(40);
+    text("GAME OVER", width / 2 - 120, height / 2);
   }
 }
 
@@ -56,6 +55,7 @@ function keyPressed() {
 }
 
 function startGame() {
+
   score = 0;
   health = 100;
 
@@ -64,32 +64,32 @@ function startGame() {
   if (badItems) badItems.remove();
   if (obstacles) obstacles.remove();
 
-  player = new Sprite(100, 100, 40, 40);
-  player.img = snakeImg;
-  player.scale = 0.2;
+  // player
+  player = new Sprite(width / 2, height / 2, 40, 40);
+  player.img = playerImg;
+  player.scale = 0.3;
 
-  apple = new Sprite(random(100, 700), random(100, 500), 20, 20);
+  // apple
+  apple = new Sprite(random(100, width - 100), random(100, height - 100), 20, 20);
   apple.img = appleImg;
-  apple.scale = 0.2;
+  apple.scale = 0.3;
 
   badItems = new Group();
   obstacles = new Group();
 
   // bad items
   for (let i = 0; i < 3; i++) {
-    let b = new Sprite(random(100, 700), random(100, 500), 30, 30);
-    b.img = burgerImg;
-    b.scale = 0.1;
-
+    let b = new Sprite(random(100, width - 100), random(100, height - 100), 30, 30);
+    b.img = badImg;
+    b.scale = 0.2;
     b.vel.x = random([-2, 2]);
     b.vel.y = random([-2, 2]);
-
     badItems.add(b);
   }
 
   // obstacles
   for (let i = 0; i < 3; i++) {
-    let o = new Sprite(random(150, 650), random(150, 450), 100, 30);
+    let o = new Sprite(random(150, width - 150), random(150, height - 150), 100, 30);
     o.color = "gray";
     o.collider = "static";
     obstacles.add(o);
@@ -111,11 +111,11 @@ function runGame() {
   // collect apple
   if (player.overlaps(apple)) {
     score++;
-    apple.pos.x = random(100, 700);
-    apple.pos.y = random(100, 500);
+    apple.pos.x = random(100, width - 100);
+    apple.pos.y = random(100, height - 100);
   }
 
-  // hit bad item
+  // hit bad items
   for (let b of badItems) {
     if (player.overlaps(b)) {
       health -= 1;
@@ -125,9 +125,9 @@ function runGame() {
   drawSprites();
 
   fill(255);
-  textSize(16);
-  text("Score: " + score, 20, 20);
-  text("Health: " + health, 20, 40);
+  textSize(18);
+  text("Score: " + score, 20, 30);
+  text("Health: " + health, 20, 60);
 
   if (score >= 10) {
     gameState = "win";
@@ -139,13 +139,17 @@ function runGame() {
 }
 
 function movePlayer() {
-  let speed = 3;
+  let speed = 4;
 
   player.vel.x = 0;
   player.vel.y = 0;
 
-  if (kb.pressing("right")) player.vel.x = speed;
-  if (kb.pressing("left")) player.vel.x = -speed;
   if (kb.pressing("up")) player.vel.y = -speed;
   if (kb.pressing("down")) player.vel.y = speed;
+  if (kb.pressing("left")) player.vel.x = -speed;
+  if (kb.pressing("right")) player.vel.x = speed;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
