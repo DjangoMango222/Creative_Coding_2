3c0c833af4aaf004328b1540e6a3b8d2c0106025
Django Behunin <django.behunin@umconnect.umt.edu>
2026-03-21 23:11:11 -0600

    asdf

--- HW-9/js/sketch.js
+++ HW-9/js/sketch.js
@@ -3,22 +3,15 @@ let apple;
 let badItems;
 let obstacles;
 
-let snakeAnim;
 let appleImg, burgerImg;
+let snakeImg;
 
 let score = 0;
 let health = 100;
 let gameState = "menu";
 
 function preload() {
-
-  snakeAnim = loadAnimation(
-    "images/snake-head-right.png",
-    "images/snake-head-up.png",
-    "images/snake-head-left.png",
-    "images/snake-head-down.png"
-  );
-
+  snakeImg = loadImage("images/snake-head-right.png");
   appleImg = loadImage("images/apple.png");
   burgerImg = loadImage("images/fast-food-png-41613.png");
 }
@@ -30,19 +23,30 @@ function setup() {
 function draw() {
   background(30);
 
-  if (gameState === "menu") drawMenu();
-  if (gameState === "play") playGame();
-  if (gameState === "win") drawWin();
-  if (gameState === "lose") drawLose();
-}
+  if (gameState === "menu") {
+    fill(255);
+    textSize(40);
+    text("VentureSnake", 240, 200);
 
-function drawMenu() {
-  fill(255);
-  textSize(40);
-  text("VentureSnake", 240, 200);
+    textSize(18);
+    text("Press SPACE to start", 280, 260);
+  }
 
-  textSize(18);
-  text("Press SPACE to start", 280, 260);
+  if (gameState === "play") {
+    runGame();
+  }
+
+  if (gameState === "win") {
+    fill(255);
+    textSize(30);
+    text("YOU WIN", 320, 300);
+  }
+
+  if (gameState === "lose") {
+    fill(255);
+    textSize(30);
+    text("GAME OVER", 300, 300);
+  }
 }
 
 function keyPressed() {
@@ -54,58 +58,69 @@ function keyPressed() {
 function startGame() {
   score = 0;
   health = 100;
-  setupGame();
-  gameState = "play";
-}
-
-function setupGame() {
 
   if (player) player.remove();
   if (apple) apple.remove();
-  if (badItems) badItems.removeSprites();
-  if (obstacles) obstacles.removeSprites();
+  if (badItems) badItems.remove();
+  if (obstacles) obstacles.remove();
 
-  player = createSprite(100, 100, 40, 40);
+  player = new Sprite(100, 100, 40, 40);
+  player.img = snakeImg;
+  player.scale = 0.2;
 
-  player.addAnimation("move", snakeAnim);
-  player.scale = 1.1;
-
-  apple = createSprite(random(100, 700), random(100, 500), 20, 20);
-  apple.addImage(appleImg);
-  apple.scale = 0.5;
+  apple = new Sprite(random(100, 700), random(100, 500), 20, 20);
+  apple.img = appleImg;
+  apple.scale = 0.2;
 
   badItems = new Group();
   obstacles = new Group();
 
+  // bad items
   for (let i = 0; i < 3; i++) {
-    let b = createSprite(random(100, 700), random(100, 500), 30, 30);
-    b.addImage(burgerImg);
-    b.scale = 0.2;
-    b.velocity.x = random([-2, 2]);
-    b.velocity.y = random([-2, 2]);
+    let b = new Sprite(random(100, 700), random(100, 500), 30, 30);
+    b.img = burgerImg;
+    b.scale = 0.1;
+
+    b.vel.x = random([-2, 2]);
+    b.vel.y = random([-2, 2]);
+
     badItems.add(b);
   }
 
-  // obstacles 
+  // obstacles
   for (let i = 0; i < 3; i++) {
-    let o = createSprite(random(150, 650), random(150, 450), 100, 30);
-    o.immovable = true;
+    let o = new Sprite(random(150, 650), random(150, 450), 100, 30);
+    o.color = "gray";
+    o.collider = "static";
     obstacles.add(o);
   }
+
+  gameState = "play";
 }
 
-function playGame() {
+function runGame() {
 
   movePlayer();
 
-  player.collide(obstacles);
+  player.collides(obstacles);
 
-  for (let i = 0; i < badItems.length; i++) {
-    badItems[i].bounceOff(obstacles);
+  for (let b of badItems) {
+    b.collides(obstacles);
   }
 
-  player.overlap(apple, collectApple);
-  player.overlap(badItems, hitBad);
+  // collect apple
+  if (player.overlaps(apple)) {
+    score++;
+    apple.pos.x = random(100, 700);
+    apple.pos.y = random(100, 500);
+  }
+
+  // hit bad item
+  for (let b of badItems) {
+    if (player.overlaps(b)) {
+      health -= 1;
+    }
+  }
 
   drawSprites();
 
@@ -114,49 +129,23 @@ function playGame() {
   text("Score: " + score, 20, 20);
   text("Health: " + health, 20, 40);
 
-  if (score >= 10) gameState = "win";
-  if (health <= 0) gameState = "lose";
+  if (score >= 10) {
+    gameState = "win";
+  }
+
+  if (health <= 0) {
+    gameState = "lose";
+  }
 }
 
 function movePlayer() {
   let speed = 3;
 
-  if (keyDown("UP_ARROW")) {
-    player.velocity.x = 0;
-    player.velocity.y = -speed;
-  }
-  else if (keyDown("DOWN_ARROW")) {
-    player.velocity.x = 0;
-    player.velocity.y = speed;
-  }
-  else if (keyDown("LEFT_ARROW")) {
-    player.velocity.x = -speed;
-    player.velocity.y = 0;
-  }
-  else if (keyDown("RIGHT_ARROW")) {
-    player.velocity.x = speed;
-    player.velocity.y = 0;
-  }
-}
+  player.vel.x = 0;
+  player.vel.y = 0;
 
-function collectApple(p, a) {
-  score++;
-  a.position.x = random(100, 700);
-  a.position.y = random(100, 500);
-}
-
-function hitBad(p, b) {
-  health -= 1;
-}
-
-function drawWin() {
-  fill(255);
-  textSize(30);
-  text("YOU WIN", 320, 300);
-}
-
-function drawLose() {
-  fill(255);
-  textSize(30);
-  text("GAME OVER", 300, 300);
+  if (kb.pressing("right")) player.vel.x = speed;
+  if (kb.pressing("left")) player.vel.x = -speed;
+  if (kb.pressing("up")) player.vel.y = -speed;
+  if (kb.pressing("down")) player.vel.y = speed;
 }
