86a8c171787e9ad30bbd01347aebe2dc8d9ba333
Django Behunin <django.behunin@umconnect.umt.edu>
2026-04-06 12:41:32 -0600

    asdf

--- HW-12/sketch.js
+++ HW-12/sketch.js
@@ -1,102 +1,118 @@
-let textTexture;
+let titleTexture;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  textFont('Georgia');
 
-  textTexture = createGraphics(512, 256);
-  textTexture.background(20, 10, 40);
-  textTexture.fill(255);
-  textTexture.textAlign(CENTER, CENTER);
+  titleTexture = createGraphics(700, 220);
+  titleTexture.clear();
+  titleTexture.background(18, 12, 40);
 
-  textTexture.textSize(32);
-  textTexture.textStyle(BOLD);
-  textTexture.text("Cosmic Sculpture Garden", 256, 95);
+  titleTexture.fill(40, 30, 80);
+  titleTexture.rect(10, 10, 680, 200, 18);
 
-  textTexture.textSize(24);
-  textTexture.textStyle(NORMAL);
-  textTexture.text("by Dj", 256, 155);
+  titleTexture.fill(255);
+  titleTexture.textAlign(CENTER, CENTER);
+
+  titleTexture.textSize(40);
+  titleTexture.textStyle(BOLD);
+  titleTexture.text("DJ's 3D Creations", 350, 85);
+
+  titleTexture.textSize(26);
+  titleTexture.textStyle(NORMAL);
+  titleTexture.fill(220);
+  titleTexture.text("by Dj", 350, 145);
 }
 
 function draw() {
-  background(8, 6, 18);
+  background(6, 8, 25);
 
   orbitControl();
 
-  ambientLight(70);
-  directionalLight(255, 255, 255, -0.5, 0.7, -1);
-  pointLight(180, 180, 255, 0, -200, 250);
+  ambientLight(60, 60, 70);
+  directionalLight(255, 255, 255, -0.4, 0.6, -1);
+  pointLight(160, 180, 255, 0, -120, 220);
+  pointLight(255, 120, 180, -220, 80, 120);
 
-  // Ground
   push();
+  translate(0, 170, 0);
   rotateX(HALF_PI);
-  ambientMaterial(40, 70, 120);
-  translate(0, 0, 180);
-  plane(1000, 1000);
+  ambientMaterial(35, 45, 80);
+  plane(700, 700);
   pop();
 
-  // 1 box
   push();
-  translate(-250, -100, 0);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.015);
-  ambientMaterial(255, 120, 120);
-  box(110);
+  translate(0, -220, -120);
+  rotateY(sin(frameCount * 0.01) * 0.2);
+  texture(titleTexture);
+  plane(320, 100);
   pop();
 
-  // 2 sphere
   push();
-  translate(0, -140, 0);
-  rotateY(frameCount * 0.02);
-  rotateZ(frameCount * 0.01);
+  translate(0, 30, 0);
+  rotateY(frameCount * 0.012);
+  rotateX(frameCount * 0.008);
   specularMaterial(120, 220, 255);
-  shininess(80);
-  sphere(75);
+  shininess(100);
+  sphere(65);
   pop();
 
-  // 3 cone
   push();
-  translate(240, -80, 0);
+  translate(-180, 40, -60);
+  rotateX(frameCount * 0.014);
+  rotateY(frameCount * 0.018);
+  ambientMaterial(255, 140, 140);
+  box(95);
+  pop();
+
+  push();
+  translate(190, 35, -40);
   rotateX(frameCount * 0.02);
   rotateZ(frameCount * 0.015);
   normalMaterial();
-  cone(70, 140);
+  cone(55, 120);
   pop();
 
-  // 4 cylinder
   push();
-  translate(-180, 140, -100);
-  rotateY(frameCount * 0.017);
-  rotateX(frameCount * 0.012);
-  specularMaterial(255, 220, 120);
-  shininess(40);
-  cylinder(55, 130);
-  pop();
-
-  // 5 torus
-  push();
-  translate(120, 120, 60);
-  rotateX(frameCount * 0.018);
+  translate(-120, 95, 150);
   rotateY(frameCount * 0.02);
-  ambientMaterial(180, 120, 255);
-  torus(70, 20);
+  rotateZ(frameCount * 0.01);
+  specularMaterial(255, 220, 140);
+  shininess(60);
+  cylinder(40, 110);
   pop();
 
-  // 6 ellipsoid
   push();
-  translate(320, 120, -120);
-  rotateY(frameCount * 0.014);
+  translate(130, 100, 130);
+  rotateX(frameCount * 0.02);
+  rotateY(frameCount * 0.018);
+  ambientMaterial(180, 120, 255);
+  torus(58, 16);
+  pop();
+
+  push();
+  translate(135, -35, -160);
+  rotateY(frameCount * 0.016);
   rotateZ(frameCount * 0.02);
-  specularMaterial(120, 255, 170);
-  shininess(100);
+  specularMaterial(130, 255, 170);
+  shininess(90);
   ellipsoid(90, 55, 55);
   pop();
 
-  // title + name panel
   push();
-  translate(0, -260, -200);
-  rotateY(sin(frameCount * 0.01) * 0.3);
-  texture(textTexture);
-  plane(360, 180);
+  translate(-230, -20, 120);
+  rotateY(frameCount * 0.03);
+  ambientMaterial(120, 170, 255);
+  sphere(35);
+  pop();
+
+  push();
+  translate(0, -40, 180);
+  rotateX(frameCount * 0.015);
+  rotateY(frameCount * 0.022);
+  specularMaterial(255, 170, 210);
+  shininess(80);
+  torus(38, 10);
   pop();
 }
 
